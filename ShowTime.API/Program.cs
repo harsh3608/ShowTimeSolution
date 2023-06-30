70fc45c188f1b6f19c40fe19bce23a0557df4f8f
Harsh Patel <harsh3608@gmail.com>
2023-06-30 13:11:22 +0530

    added base classes for user add and login

--- ShowTime.API/Program.cs
+++ ShowTime.API/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using ShowTime.Core.IdentityEntities;
 using ShowTime.Infrastructure.DatabaseContext;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,12 +13,30 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
 
+
 // Add services to the container.
+// Service Scopes
+builder.Services.AddTransient<IJwtService, JwtService>();
+
+//Identity
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireDigit = true;
+
+}).AddEntityFrameworkStores<ApplicationDbContext>()
+  .AddDefaultTokenProviders()
+  .AddUserStore<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, Guid>>()
+  .AddRoleStore<RoleStore<ApplicationRole, ApplicationDbContext, Guid>>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
---
+++ ShowTime.Core/Models/UserLoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowTime.Core.Models
+{
+    internal class UserLoginRequest
+    {
+    }
+}
