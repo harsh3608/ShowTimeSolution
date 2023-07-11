54f45041fa9d5cd2226444c06ea659bc135b746a
Harsh Patel <harsh3608@gmail.com>
2023-07-11 18:14:58 +0530

    added leave entity

---
+++ ShowTime.Core/Enums/LeaveTypeOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowTime.Core.Enums
+{
+    public enum LeaveTypeOptions
+    {
+        CompensatoryOff = 0,
+        EarnedLeave = 1,
+        FloatingHoliday = 2,
+        LeaveWithoutPay = 3,
+        WorkFromHome = 4
+    }
+}
---
+++ ShowTime.Core/Models/LeaveAddRequest.cs
@@ -0,0 +1,26 @@
+﻿using ShowTime.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowTime.Core.Models
+{
+    public class LeaveAddRequest
+    {
+        public Guid UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Reason { get; set; }
+
+        public LeaveTypeOptions LeaveType { get; set; }
+
+        public bool IsApproved { get; set; }
+    }
+}
